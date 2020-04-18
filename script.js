9e76b56f5c755e8b0564c2011ef7dcd61e518561
iphilka <i376765616@yandex.ru>
2020-04-18 17:16:27 +0500

    js update

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
 "use strict";
 
+let vkLogin = document.querySelector("#vkLogin");
+console.log(vkLogin);
+
+
 VK.init({
     apiId: 7417722
+  });
+
+vkLogin.addEventListener('click', function () {
+ 
+    // Авторизация
+    VK.Auth.login(
+      // callback-функция, которая будет вызвана после авторизации
+      function (response) {
+ 
+        console.log(response)
+ 
+        if (response.status === 'connected') { // авторизация прошла успешно
+ 
+          var user = response.session.user; //  информация о пользователе
+            /*
+             user.first_name - имя;
+             user.last_name - фамилия;
+             user.href - ссылка на страницу в формате https://vk.com/domain;
+             user.id  - идентификатор пользователя;
+             user.nickname -  отчество или никнейм (если указано);
+             */
+ 
+        } else if (response.status === 'not_authorized') { // пользователь авторизован в ВКонтакте, но не разрешил доступ приложению;
+ 
+        } else if (response.status === 'unknown') { // пользователь не авторизован ВКонтакте.
+ 
+        }
+ 
+      },
+      // права доступа (integer)
+      // допустимые значения:
+      // AUDIO:8
+      // FRIENDS:2
+      // MATCHES:32
+      // PHOTOS:4
+      // QUESTIONS:64
+      // VIDEO:16
+      // WIKI:128
+      VK.access.PHOTOS
+    );
+ 
   });
